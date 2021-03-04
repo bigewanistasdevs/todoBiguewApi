8abfdb792241fa3a2cdedd80c1f572b12f389ff8
Alison Lovegood <alisondlovegood@gmail.com>
2021-03-04 20:32:01 -0300

    teste 1.0

--- app/Models/Lista.php
+++ app/Models/Lista.php
@@ -2,27 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Tarefa;
 
 class Lista extends Model
 {
-    // use HasFactory;
+    use HasFactory;
 
     // testando método de modelo com a tarefa como ligação e com fillable
-    protected $fillable=[
-        'matricula',
-        'nome',
-        'concluida'
-    ];
+    // protected $fillable=[
+    //     'matricula',
+    //     'nome',
+    //     'concluida'
+    // ];
 
     public function tarefas(){
         return $this->hasMany(Tarefa::class);
     }
 
 }
-
-
-// class Lista extends Authenticatable{
-    
-// }
--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -2,26 +2,23 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Lista;
 
 class Tarefa extends Model
 {
-    // use HasFactory;
+    use HasFactory;
 
     // p fazer a ligação com as listas, N SEI SE ESTÁ CORRETO
-    protected $fillable=[
-        'titulo',
-        'descricao',
-        'concluida',
-    ];
+    // protected $fillable=[
+    //     'titulo',
+    //     'descricao',
+    //     'concluida',
+    // ];
 
     public function lista(){
         return $this->belongsTo(Lista::class);  
     }
 }
-
-
-// class Tarefa extends Authenticatable {
-
-// }
