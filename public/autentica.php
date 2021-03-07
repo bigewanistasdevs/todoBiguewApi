4529620c4142c922d57c06163b3434dc7130f306
YwrSch <ywrygalvao@gmail.com>
2021-03-06 21:02:28 -0300

    login SUAP

--- public/autentica.php
+++ public/autentica.php
@@ -28,43 +28,30 @@ if(isset($_POST['matricula'])){
                 $n_disciplinas = count($turmasVirtuais);
                 $boletins = $suap->getMeuBoletim($ano -1, 1);
 
-                //PEGA FALTAS DAS DISCIOLINAS
+                //PEGA INFORMAÇÕES DAS DISCIOLINAS
                 $faltas = array();
+                $disciplinas = array();
+                $situacoesDisciplinas = array();
+                $cargasHorarias = array();
+                $cargasHorariasC = array();
                 for ($i = 1; $i <= $n_disciplinas; $i++) {
                     $falta = $boletins[$i-1]['numero_faltas'];
                     $faltas += [$boletins[$i-1]['codigo_diario'] => $falta];
-                };
 
-                //PEGA NOMES DAS DISCIPLINAS
-                $disciplinas = array();
-                for ($i = 1; $i <= $n_disciplinas; $i++) {
                     $dici = $boletins[$i-1]['disciplina'];
                     $disciplinas += [$boletins[$i-1]['codigo_diario'] => $dici];
-                };
 
-                //PEGA SITUAÇÕES DAS DISCIPLINAS
-                $situacoesDisciplinas = array();
-                for ($i = 1; $i <= $n_disciplinas; $i++) {
                     $situ = $boletins[$i-1]['situacao'];
                     $situacoesDisciplinas += [$boletins[$i-1]['codigo_diario'] => $situ];
-                };
 
-                //PEGA CARGA HORARIA DAS DISCIPLINAS
-                $cargasHorarias = array();
-                for ($i = 1; $i <= $n_disciplinas; $i++) {
                     $carga = $boletins[$i-1]['carga_horaria'];
                     $cargasHorarias += [$boletins[$i-1]['codigo_diario'] => $carga];
-                };
 
-                //PEGA CARGA HORARIA CUMPRIDA DAS DISCIPLINAS
-                $cargasHorariasC = array();
-                for ($i = 1; $i <= $n_disciplinas; $i++) {
                     $cargaC = $boletins[$i-1]['carga_horaria_cumprida'];
                     $cargasHorariasC += [$boletins[$i-1]['codigo_diario'] => $carga];
                 };
 
                 //PEGA PROFESSORES COM SEUS RESPECTIVOS NOMES E FOTOS
-                $professores = array();
                 $professores_nomes = array();
                 $professores_fotos = array();
                 $disciplinasTurmas = array();
@@ -74,10 +61,6 @@ if(isset($_POST['matricula'])){
                     $disciplina = $sigla." - ".$nome;
                     for ($j = 1; $j <= $n_disciplinas; $j++) {
                         if($disciplina==$disciplinas[$boletins[$j-1]['codigo_diario']]){
-                            $prof = $suap->getTurmaVirtual($turmasVirtuais[$i-1]['id']);
-                            $proff = $prof['professores'][0]['matricula'];
-                            $professores += [$boletins[$j-1]['codigo_diario'] => $proff];
-
                             $prof_n = $suap->getTurmaVirtual($turmasVirtuais[$i-1]['id']);
                             $profn = $prof_n['professores'][0]['nome'];
                             $professores_nomes += [$boletins[$j-1]['codigo_diario'] => $profn];
@@ -88,23 +71,37 @@ if(isset($_POST['matricula'])){
                         };
                     };
                 };
-
-                $_SESSION['senha'] = $senha;
-                $_SESSION['matricula'] = $matricula;
-                $_SESSION['nome'] = $meusDados["nome_usual"];
-                $_SESSION['email_aluno'] = $meusDados["email"];
-                $_SESSION['vinculo'] = $meusDados["vinculo"]["situacao"];
-                $_SESSION['foto_url'] = $ft;
-                $_SESSION['disciplinas'] = $boletins;
-                $_SESSION['disciplinas_nomes'] = $disciplinas;
-                $_SESSION['disciplinas_cargas_horarias'] = $cargasHorarias;
-                $_SESSION['disciplinas_cargas_horarias_cumpridas'] = $cargasHorariasC;
-                $_SESSION['disciplinas_faltas'] = $faltas;
-                $_SESSION['disciplinas_situacoes'] = $situacoesDisciplinas;
-                $_SESSION['professores'] = $professores;
-                $_SESSION['professores_nomes'] = $professores_nomes;
-                $_SESSION['professores_fotos'] = $professores_fotos;
-                $_SESSION['numero_disciplinas'] = $n_disciplinas;
+                $nome = utf8_encode($meusDados["nome_usual"]);
+                $dadosUsuario = array(
+                    "matricula" => $matricula,
+                    "nome" => $nome,
+                    "email" => $meusDados["email"],
+                    "vinculo" => $meusDados["vinculo"]["situacao"],
+                    "foto" => $ft,
+                    "numero_disciplinas" => $n_disciplinas,
+                    "disciplinas" => array(
+                        "codigo_diario" => $disciplinas
+                    ),
+                    "disciplinas_cargas_horarias" => array(
+                        "codigo_diario" => $cargasHorarias
+                    ),
+                    "disciplinas_cargas_horarias_cumpridas" => array(
+                        "codigo_diario" => $cargasHorariasC
+                    ),
+                    "disciplinas_faltas" => array(
+                        "codigo_diario" => $faltas
+                    ),
+                    "disciplinas_situacoes" => array(
+                        "codigo_diario" => $situacoesDisciplinas
+                    ),
+                    "professores_nomes" => array(
+                        "codigo_diario" => $professores_nomes
+                    ),
+                    "professores" => array(
+                        "codigo_diario" => $professores_fotos
+                    ),
+                );
+                echo json_encode($dadosUsuario);
 
             }elseif(strlen($matricula) == 7){
                 
@@ -113,26 +110,11 @@ if(isset($_POST['matricula'])){
                 $_SESSION['PROF'] == true;
 
             };
-            echo "Logado, olá ".$_SESSION['nome'];
+            exit;
         };
         //AQUI MANDA UM ELSE PARA OS PROFESSORES;;
     } catch (Exception $e) {
         echo "Não logado";
+        return false;
     };
 };
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-    <form action="?" method="POST">
-        Matrícula: <input type="text" name="matricula">
-        Senha: <input type="password" name="senha">
-        <input type="submit" value="vai">
-    </form>
-</body>
-</html>
