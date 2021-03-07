6e3eaf4112ba5193ccfd94dfd9ee57b2a9994900
Alison Lovegood <alisondlovegood@gmail.com>
2021-03-07 09:53:52 -0300

    rs

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -12,12 +12,12 @@ class Tarefa extends Model
     use HasFactory;
 
     // p fazer a ligação com as listas, N SEI SE ESTÁ CORRETO
-    // protected $fillable=[
-    //     'titulo',
-    //     'descricao',
-    //     'lista_id',
-    //     'concluida',
-    // ];
+    protected $fillable=[
+        'titulo',
+        'descricao',
+        'lista_id',
+        'concluida',
+    ];
 
     public function lista(){
         return $this->belongsTo(Lista::class);  
