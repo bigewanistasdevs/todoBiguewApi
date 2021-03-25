f68da21ec2c8fda12ee0b5d791394b9ebd01b1b0
Deyvid William <deyvidwms@gmail.com>
2021-03-25 08:31:36 -0300

    Teste 5.0

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
+    Route::apiResource('listas', 'App\Http\Controllers\api\ListaController');
+    Route::apiResource('tarefas', 'App\Http\Controllers\api\TarefaController');
+    Route::apiResource('autentica', 'App\Http\Controllers\api\AutenticaController');
     
-    Route::group(['middleware' => 'cors'], function () {
-        Route::apiResource('listas', 'App\Http\Controllers\api\ListaController');
-        Route::apiResource('tarefas', 'App\Http\Controllers\api\TarefaController');
-        Route::apiResource('autentica', 'App\Http\Controllers\api\AutenticaController');
-    });
+     
+    // Route::group(['middleware' => 'cors'], function () {
+    //     Route::apiResource('listas', 'App\Http\Controllers\api\ListaController');
+    //     Route::apiResource('tarefas', 'App\Http\Controllers\api\TarefaController');
+    //     Route::apiResource('autentica', 'App\Http\Controllers\api\AutenticaController');
+    // });
